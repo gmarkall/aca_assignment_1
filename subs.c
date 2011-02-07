7884d9483a352d907dda1124ef1ed887e68f42e0
gmarkall <grm08@doc.ic.ac.uk>
2011-02-07 23:12:01 +0000

    Add subs.c, complete build, user -Wall -Werror

---
+++ subs.c
@@ -0,0 +1,20 @@
+// subs.c
+
+#include "subs.h"
+
+void distribute_particles_randomly()
+{
+
+}
+
+void output_positions(int file_index)
+{
+
+}
+
+int particlepos(int grav_fac, int dt_fac, int min_threshold)
+{
+  int iterations = 0;
+
+  return iterations;
+}
