0adbd002e4342135d0fb8c7ab16ab5fc1f79ea7e
gmarkall <grm08@doc.ic.ac.uk>
2011-02-07 23:33:57 +0000

    Add random seed. Add global variables from fortran code

--- subs.c
+++ subs.c
@@ -2,6 +2,22 @@
 
 #include "subs.h"
 
+static double* pparticles;           /* Particle x, y coordinates    */
+static double* pparticlesnew;        /* Updated particle coordinates */
+static double* fparticles;           /* Interparticle forces         */
+static double* vparticles;           /* Particle velocities          */
+static double* prad;                 /* Particle radii               */
+static double* pradstart;            /* Initial particle radii       */
+
+static int numparticles;             /* Number of particles          */
+static int rseed;                    /* Random seed                  */
+
+static double springkrepel;          /* Global repulsive forces      */
+static double stddevfac;             /* Std deviation factor         */
+static double lowerprad;             /* Particle radius lower bound  */
+static double upperprad;             /* Particle radius upper bound  */
+static double particlerad;           /* Mean particle radius         */
+
 void distribute_particles_randomly()
 {
 
